189e522e93949dc2d45a23659cbae34ace3f83ad
yonin7 <yonin789@walla.com>
2021-12-19 16:55:25 +0200

    second upload

--- 12.1/src/App.js
+++ 12.1/src/App.js
@@ -8,11 +8,22 @@ import './App.css';
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { joke: '', categories: [] };
+    this.state = {
+      joke: '',
+      categoryJoke: '',
+      categories: [],
+      errorMessage: '',
+      isLoading: false,
+    };
   }
   onClickSubmit = async () => {
-    const joke = await JokeAPI.get('jokes/random');
-    this.setState({ joke: joke.data.value });
+    try {
+      const joke = await JokeAPI.get('jokes/random');
+      this.setState({ joke: joke.data.value });
+    } catch (e) {
+      const message = e.response.data.message;
+      this.setState({ errorMessage: message });
+    }
   };
   onClickCategory = async () => {
     const categories = await JokeAPI.get('jokes/categories');
@@ -20,20 +31,33 @@ class App extends React.Component {
     this.setState({ ...this.state, categories: categories.data });
     console.log(this.state);
   };
+  categoryJoke = async (category) => {
+    try {
+      const joke = await JokeAPI.get(`jokes/random?category=${category}`);
+      this.setState({ joke: joke.data.value });
+    } catch (e) {
+      const message = e.response.data.message;
+      this.setState({ errorMessage: message });
+    }
+  };
   categoriesButtons = () => {
     console.log(this.state.categories);
     return this.state.categories.map((category, i) => (
-      <button key={i}>{category}</button>
+      <button key={i} onClick={() => categoryJoke(category)}>
+        {category}
+      </button>
     ));
   };
 
   render() {
+    const { joke, errorMessage } = this.state;
     return (
       <div className="App">
         <Button onClick={this.onClickSubmit} />
-        <Joke joke={this.state.joke} />
+        <Joke joke={this.state} />
         <button onClick={this.onClickCategory}>categories</button>
         <div>{this.categoriesButtons()}</div>
+        <p>{this.categoryJoke}</p>
       </div>
     );
   }
